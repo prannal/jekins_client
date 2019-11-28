0fbf26e1cf5b47ec1b25ee4e8091d623aa03277b
prannal <pranjalinal06@gmail.com>
2019-11-28 17:36:43 +0530

    hi

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -13,24 +13,24 @@ export class DataService {
 
    select()
    {
-     return this.helper.get("http://13.235.94.164:3434/world/");
+     return this.helper.get("http://localhost:3434/world/");
    }
 
    selectByNo(id)
    {
-    return this.helper.get("http://13.235.94.164:3434/world/"+id);
+    return this.helper.get("http://localhost:3434/world/"+id);
 
    }
 
    insert(worldObj)
    {
-    return this.helper.post("http://13.235.94.164:3434/world/",worldObj);
+    return this.helper.post("http://localhost:3434/world/",worldObj);
 
    }
 
    delete(id)
    {
-    return this.helper.delete("http://13.235.94.164:3434/world/"+id);
+    return this.helper.delete("http://localhost:3434/world/"+id);
 
    }
 
